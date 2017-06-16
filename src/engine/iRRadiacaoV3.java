1a8212edab5ffaf0a0230134e9b435666736aa49
ProjetoInterdisciplinarFANAP <projeto.interdisciplina.fanap@gmail.com>
2017-06-16 13:08:55 -0300

    . Fazendo primeiro Commit. .

---
+++ src/engine/iRRadiacaoV3.java
@@ -0,0 +1,12 @@
+package engine;
+
+/**
+ *
+ * @author Kaichiro Fukuda
+ */
+public class iRRadiacaoV3 {
+
+    public static void main(String[] abc) {
+        System.out.println("Iniciando projeto V3");
+    }
+}
